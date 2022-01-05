92c1166c4f0183870e74c563087744d2f40a03d5
fanxiangshangfen <315011850@qq.com>
2022-01-05 19:00:31 +0800

    Update install_common_deps.sh
    更改解压命令

--- dockerfiles/scripts/install_common_deps.sh
+++ dockerfiles/scripts/install_common_deps.sh
@@ -22,5 +22,5 @@ rm -rf /opt/miniconda/pkgs
 # wget --quiet https://github.com/Kitware/CMake/releases/download/v3.21.0/cmake-3.21.0-linux-x86_64.tar.gz
 wget https://cmake.org/files/v3.21/cmake-3.21.0-linux-x86_64.tar.gz --no-check-certificate
 # tar zxf cmake-3.21.0-linux-x86_64.tar.gz
-tar -zcvf cmake-3.21.0-linux-x86_64.tar.gz
+tar -zxvf cmake-3.21.0-linux-x86_64.tar.gz
 rm -rf cmake-3.21.0-linux-x86_64.tar.gz
